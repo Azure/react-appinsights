13454f3641def52b0ebd7830370bd1ff74c807e3
Anastasiia Zolochevska <anastasiia.zolochevska@gmail.com>
2016-09-21 19:19:27 -0700

    Remove ComponentDidMount event handling

--- src/components/TrackedComponent.js
+++ src/components/TrackedComponent.js
@@ -11,38 +11,29 @@ export const TrackedComponent = React.createClass({
   },
 
   componentWillMount:function (){
-    this.componentWillMountTime = Date.now();
+    this.componentWillMountTimestamp = Date.now();
+    this.idleTimeInMs = 0;
     var timer = away(30000);
     var self=this;
     timer.on('idle', function() {
         self.startIdleTimer = Date.now();
     });
     timer.on('active', function() {
-        self.idleTimeInMs = self.idleTimeInMs?self.idleTimeInMs:0+(Date.now()-self.startIdleTimer);
+        self.idleTimeInMs += (Date.now()-self.startIdleTimer);
     });
   },
 
-  componentDidMount:function (){
-    if(this.componentWillMountTime){
-      AppInsights.trackMetric(
-        "React Component load time", 
-        Date.now() - this.componentWillMountTime,
+  componentWillUnmount: function(){
+     if(!this.componentWillMountTimestamp){
+       throw "ComponentWillMountTimestamp was not initialized. Check if super.componentWillMount() was called"
+     }
+     AppInsights.trackMetric(
+        "React Component Engaged Time (seconds)", 
+        (Date.now() - this.componentWillMountTimestamp - (this.idleTimeInMs?this.idleTimeInMs:0))/1000,
         1,
         null,
         null,
-        {'Component Name':this.constructor.displayName});
-    }
-  },
-
-  componentWillUnmount: function(){
-     if(this.componentWillMountTime){
-        AppInsights.trackMetric(
-          "React Component Engaged Time", 
-          Date.now() - this.componentWillMountTime - (this.idleTimeInMs?this.idleTimeInMs:0),
-          1,
-          null,
-          null,
-          {'Component Name' : this.constructor.displayName});
-     }
+        {'Component Name' : this.constructor.displayName});
+     
   }
 });
