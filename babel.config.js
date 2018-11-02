9b7b5007365f53fa0630a263736051246299d88d
Paolo Viotti <pviotti@microsoft.com>
2018-11-02 11:38:19 +0000

    rm webpack; add rollup build config

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,16 @@
-module.exports={
-  "presets": [ "@babel/preset-env", "@babel/preset-react"],
-  "env": {
+module.exports = function (api) {
+  api.cache(true);
+  const presets = [  "@babel/preset-env", "@babel/preset-react" ];
+  const env = {
     "test": {
       "plugins": [
         "transform-amd-to-commonjs"
       ]
     }
-}
+  }
+
+  return {
+    presets,
+    env
+  };
 }
---
+++ rollup.base.config.js
@@ -0,0 +1,107 @@
+import nodeResolve from "rollup-plugin-node-resolve";
+import multiEntry from "rollup-plugin-multi-entry";
+import cjs from "rollup-plugin-commonjs";
+import replace from "rollup-plugin-replace";
+import { uglify } from "rollup-plugin-uglify";
+import { terser } from "rollup-plugin-terser";
+
+import nodeBuiltins from "rollup-plugin-node-builtins";
+import viz from "rollup-plugin-visualizer";
+
+const pkg = require("./package.json");
+const depNames = Object.keys(pkg.dependencies);
+const input = "dist-esm/src/index.js";
+
+export function nodeConfig(test = false) {
+  const externalNodeBuiltins = ["events"];
+  const baseConfig = {
+    input: input,
+    external: depNames.concat(externalNodeBuiltins),
+    output: { file: "dist/index.js", format: "cjs", sourcemap: true },
+    plugins: [
+      replace({
+        delimiters: ["", ""],
+        values: {
+          // replace dynamic checks with if (true) since this is for node only.
+          // Allows rollup's dead code elimination to be more aggressive.
+          "if (isNode)": "if (true)"
+        }
+      }),
+      nodeResolve({ preferBuiltins: true }),
+      cjs()
+    ]
+  };
+
+  if (test) {
+    // entry point is every test file
+    baseConfig.input = "dist-esm/test/**/*.spec.js";
+    baseConfig.plugins.unshift(multiEntry({ exports: false }));
+
+    // different output file
+    baseConfig.output.file = "test-dist/index.js";
+
+    // mark assert as external
+    baseConfig.external.push("assert");
+  } else {
+    baseConfig.plugins.push(uglify());
+  }
+
+  return baseConfig;
+}
+
+export function browserConfig(test = false) {
+  const baseConfig = {
+    input: input,
+    external: ["ms-rest-js"],
+    output: {
+      file: "browser/index.js",
+      format: "umd",
+      name: "ExampleClient",
+      sourcemap: true,
+      globals: { "ms-rest-js": "msRest" }
+    },
+    plugins: [
+      replace(
+        // ms-rest-js is externalized so users must include it prior to using this bundle.
+        {
+          delimiters: ["", ""],
+          values: {
+            // replace dynamic checks with if (false) since this is for
+            // browser only. Rollup's dead code elimination will remove
+            // any code guarded by if (isNode) { ... }
+            "if (isNode)": "if (false)"
+          }
+        }
+      ),
+      nodeResolve({
+        preferBuiltins: false,
+        browser: true
+      }),
+      cjs({
+        namedExports: { events: ["EventEmitter"] }
+      }),
+      nodeBuiltins(),
+      viz({ filename: "browser/browser-stats.html", sourcemap: false })
+    ],
+    onwarn
+  };
+
+  if (test) {
+    baseConfig.input = "dist-esm/test/**/*.spec.js";
+    baseConfig.plugins.unshift(multiEntry({ exports: false }));
+    baseConfig.output.file = "test-browser/index.js";
+  } else {
+    baseConfig.plugins.push(uglify());
+  }
+
+  return baseConfig;
+}
+
+function onwarn(warning, warn) {
+  if (warning.code === "CIRCULAR_DEPENDENCY" && warning.importer.indexOf("duplex.js") > -1) {
+    // circular dependencies in Stream (required by create-hmac via rollup-plugin-node-builtins)
+    // These should be ignored per https://github.com/calvinmetcalf/rollup-plugin-node-builtins/issues/39
+    return;
+  }
+  warn(warning);
+}
