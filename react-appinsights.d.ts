c95406dc22a9716ea5b34f7e27d158c807b54e4d
Paolo Viotti <pviotti@microsoft.com>
2018-11-20 10:43:53 +0000

    fix TypeScript type definition file (courtesy of AndyCJ)

--- react-appinsights.d.ts
+++ react-appinsights.d.ts
@@ -1,26 +1,24 @@
-// Type definitions for react-appinsights 1.0.5
-// Project: https://github.com/anastasiia-zolochevska/react-appinsights
-// Definitions by: Kenneth Kolstad <https://github.com/kkolstad>
-// TypeScript Version: 2.7
-declare module 'react-appinsights' {
+declare module "react-appinsights" {
+    import "applicationinsights-js";
     import * as React from 'react';
     import { History } from 'history';
-    interface IReactAI {
+
+    interface ReactAppInsights {
         init(
             appInsightsOptions: Microsoft.ApplicationInsights.IConfig,
             history: History
         ): void;
+
         ai(): Microsoft.ApplicationInsights.IAppInsights;
-        trackRouterChange(): void;
+
         setAppContext(properties: {}): void;
-        withTracking(
-            WrappedComponent: React.Component, 
-            componentName: string
-        ): React.Component;
+
+        withTracking<TProp>(
+            WrappedComponent: React.ComponentType<TProp>,
+            componentName?: string
+        ): React.ComponentType<TProp>;
     }
 
-    export class TrackedComponent<P = {}, S = {}> extends React.Component<P, S> {}
-
-    let ReactAI: IReactAI;
-    export default ReactAI;
+    const reactAppInsights: ReactAppInsights;
+    export default reactAppInsights;
 }
