f31a4fbc11b13eb12738986f31972c02f94db692
Paolo Viotti <pviotti@microsoft.com>
2018-11-20 11:48:41 +0000

    add copyright header to type file

--- react-appinsights.d.ts
+++ react-appinsights.d.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 declare module "react-appinsights" {
     import "applicationinsights-js";
     import * as React from 'react';
