8ac1f575dbd2d4d63915107f26aaf20c56d48898
Paolo Viotti <pviotti@microsoft.com>
2018-11-22 16:13:44 +0000

    add prettier with pre-commit

--- src/ReactAI.js
+++ src/ReactAI.js
@@ -2,81 +2,81 @@
 // Licensed under the MIT License.
 
 import React from 'react';
-import {AppInsights} from "applicationinsights-js"
-import AppInsightsUsage from "appinsights-usage"
+import { AppInsights } from 'applicationinsights-js';
+import AppInsightsUsage from 'appinsights-usage';
 import away from 'away';
 
-
 export const ReactAI = {
-    init(appInsightsOptions, history) {
-        AppInsights.downloadAndSetup(appInsightsOptions);
-        AppInsightsUsage.init(appInsightsOptions);
-        
-        if (history) {
-            history.listen(_ => {
-                AppInsights.trackPageView();
-            });
-        }
+  init(appInsightsOptions, history) {
+    AppInsights.downloadAndSetup(appInsightsOptions);
+    AppInsightsUsage.init(appInsightsOptions);
 
-        this.setAppContext({urlReferrer:document.referrer});
-    },
-
-    ai() {
-        return AppInsights;
-    },
-
-    setAppContext(properties) {
-        appInsights.queue.push(function () {
-            appInsights.context.addTelemetryInitializer(function (envelope) {
-                var telemetryItem = envelope.data.baseData;
-
-                // To set custom properties:
-                telemetryItem.properties = telemetryItem.properties || {};
-                for (var key in properties) {
-                    telemetryItem.properties[key] = properties[key];
-                }                
-            })
-        });
-    },
-
-    withTracking(WrappedComponent, componentName) {
-        // Note: This check can be better if Lodash is added as a dependency.
-        if(componentName === undefined || componentName === null || typeof componentName !== 'string'){
-            componentName = WrappedComponent.prototype.constructor.name;
-        }
-
-        return class extends React.Component {
-
-            componentDidMount() {
-                this.componentDidMountTimestamp = Date.now();
-                this.idleTimeInMs = 0;
-                var timer = away(30000);
-                var self=this;
-                timer.on('idle', function() {
-                    self.startIdleTimer = Date.now();
-                });
-                timer.on('active', function() {
-                    self.idleTimeInMs += Date.now() - self.startIdleTimer;
-                });
-            }
-
-            componentWillUnmount() {
-                if(!this.componentDidMountTimestamp) {
-                    throw "ComponentDidMountTimestamp was not initialized.";
-                }
-                AppInsights.trackMetric(
-                    "React Component Engaged Time (seconds)",
-                    (Date.now() - this.componentDidMountTimestamp - (this.idleTimeInMs ? this.idleTimeInMs : 0)) / 1000,
-                    1,
-                    null,
-                    null, {
-                        'Component Name': componentName
-                    });
-            }
-
-            render() {
-                return <WrappedComponent {...this.props} />;
-            }
-        };
+    if (history) {
+      history.listen(_ => {
+        AppInsights.trackPageView();
+      });
     }
-}
+
+    this.setAppContext({ urlReferrer: document.referrer });
+  },
+
+  ai() {
+    return AppInsights;
+  },
+
+  setAppContext(properties) {
+    appInsights.queue.push(function() {
+      appInsights.context.addTelemetryInitializer(function(envelope) {
+        var telemetryItem = envelope.data.baseData;
+
+        // To set custom properties:
+        telemetryItem.properties = telemetryItem.properties || {};
+        for (var key in properties) {
+          telemetryItem.properties[key] = properties[key];
+        }
+      });
+    });
+  },
+
+  withTracking(WrappedComponent, componentName) {
+    // Note: This check can be better if Lodash is added as a dependency.
+    if (componentName === undefined || componentName === null || typeof componentName !== 'string') {
+      componentName = WrappedComponent.prototype.constructor.name;
+    }
+
+    return class extends React.Component {
+      componentDidMount() {
+        this.componentDidMountTimestamp = Date.now();
+        this.idleTimeInMs = 0;
+        var timer = away(30000);
+        var self = this;
+        timer.on('idle', function() {
+          self.startIdleTimer = Date.now();
+        });
+        timer.on('active', function() {
+          self.idleTimeInMs += Date.now() - self.startIdleTimer;
+        });
+      }
+
+      componentWillUnmount() {
+        if (!this.componentDidMountTimestamp) {
+          throw 'ComponentDidMountTimestamp was not initialized.';
+        }
+        AppInsights.trackMetric(
+          'React Component Engaged Time (seconds)',
+          (Date.now() - this.componentDidMountTimestamp - (this.idleTimeInMs ? this.idleTimeInMs : 0)) / 1000,
+          1,
+          null,
+          null,
+          {
+            'Component Name': componentName,
+          },
+        );
+      }
+
+      render() {
+        return <WrappedComponent {...this.props} />;
+      }
+    };
+  },
+};
--- test/ReactAI.test.js
+++ test/ReactAI.test.js
@@ -2,61 +2,58 @@
 // Licensed under the MIT License.
 
 import React from 'react';
-import { ReactAI } from '../src/ReactAI'
+import { ReactAI } from '../src/ReactAI';
 import { mount } from 'enzyme';
 
-import { AppInsights } from "applicationinsights-js"
+import { AppInsights } from 'applicationinsights-js';
 
-
-const INNER_TEXT = Math.random().toString(36).substring(7);
+const INNER_TEXT = Math.random()
+  .toString(36)
+  .substring(7);
 let INIT_SETTINGS = {
-    instrumentationKey: 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxxx'
+  instrumentationKey: 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxxx',
 };
 
 class TestComponent extends React.Component {
-    render() {
-        return (
-            <div>{INNER_TEXT}</div>
-        );
-    }
+  render() {
+    return <div>{INNER_TEXT}</div>;
+  }
 }
 
 describe('Tracked component', () => {
-   
-    beforeEach(() => {
-        jest.mock("applicationinsights-js");
-        appInsights.downloadAndSetup = jest.fn();
-        appInsights.trackMetric = jest.fn();
-    })
+  beforeEach(() => {
+    jest.mock('applicationinsights-js');
+    appInsights.downloadAndSetup = jest.fn();
+    appInsights.trackMetric = jest.fn();
+  });
 
-    it('renders correctly', () => {
-        ReactAI.init(INIT_SETTINGS);
-        let TestComponentWithTracking = ReactAI.withTracking(TestComponent)
-        
-        const mountedComponent = mount(<TestComponentWithTracking />);
-        expect(mountedComponent.html()).toEqual('<div>' + INNER_TEXT + '</div>');
+  it('renders correctly', () => {
+    ReactAI.init(INIT_SETTINGS);
+    let TestComponentWithTracking = ReactAI.withTracking(TestComponent);
 
-        mountedComponent.unmount();
-        // TODO test that it's unmounted?
-    });
+    const mountedComponent = mount(<TestComponentWithTracking />);
+    expect(mountedComponent.html()).toEqual('<div>' + INNER_TEXT + '</div>');
 
-    it('sends tracked metrics back to AppInsights', () => {
-        ReactAI.init(INIT_SETTINGS);
-        expect(AppInsights.downloadAndSetup.mock.calls.length).toEqual(2);
-        expect(AppInsights.downloadAndSetup.mock.calls[0]).toEqual([INIT_SETTINGS])
+    mountedComponent.unmount();
+    // TODO test that it's unmounted?
+  });
 
-        let TestComponentWithTracking = ReactAI.withTracking(TestComponent)
-        const mountedComponent = mount(<TestComponentWithTracking />);
-        expect(AppInsights.trackMetric.mock.calls.length).toEqual(0);
+  it('sends tracked metrics back to AppInsights', () => {
+    ReactAI.init(INIT_SETTINGS);
+    expect(AppInsights.downloadAndSetup.mock.calls.length).toEqual(2);
+    expect(AppInsights.downloadAndSetup.mock.calls[0]).toEqual([INIT_SETTINGS]);
 
-        mountedComponent.unmount();
-        expect(AppInsights.trackMetric.mock.calls.length).toEqual(1);
-        expect(AppInsights.trackMetric.mock.calls[0][0]).toEqual('React Component Engaged Time (seconds)')
-        expect(AppInsights.trackMetric.mock.calls[0][1]).toBeLessThan(2);
-        expect(AppInsights.trackMetric.mock.calls[0][2]).toEqual(1);
-        expect(AppInsights.trackMetric.mock.calls[0][3]).toBeNull();
-        expect(AppInsights.trackMetric.mock.calls[0][4]).toBeNull();
-        expect(JSON.stringify(AppInsights.trackMetric.mock.calls[0][5])).toEqual("{\"Component Name\":\"TestComponent\"}");
-    });
-})
+    let TestComponentWithTracking = ReactAI.withTracking(TestComponent);
+    const mountedComponent = mount(<TestComponentWithTracking />);
+    expect(AppInsights.trackMetric.mock.calls.length).toEqual(0);
 
+    mountedComponent.unmount();
+    expect(AppInsights.trackMetric.mock.calls.length).toEqual(1);
+    expect(AppInsights.trackMetric.mock.calls[0][0]).toEqual('React Component Engaged Time (seconds)');
+    expect(AppInsights.trackMetric.mock.calls[0][1]).toBeLessThan(2);
+    expect(AppInsights.trackMetric.mock.calls[0][2]).toEqual(1);
+    expect(AppInsights.trackMetric.mock.calls[0][3]).toBeNull();
+    expect(AppInsights.trackMetric.mock.calls[0][4]).toBeNull();
+    expect(JSON.stringify(AppInsights.trackMetric.mock.calls[0][5])).toEqual('{"Component Name":"TestComponent"}');
+  });
+});
