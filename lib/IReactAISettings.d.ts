4eb1d44d4e459ed87f867b262a98e85ef7095c85
Hiral Desai <hidesai@microsoft.com>
2018-12-12 16:14:14 -0800

    Fixes to match anps template

--- lib/IReactAISettings.d.ts
+++
@@ -1,9 +0,0 @@
-import { History } from 'history';
-export default interface IReactAISettings {
-    instrumentationKey: string;
-    initialContext?: {
-        [key: string]: any;
-    };
-    history?: History;
-    debug?: boolean;
-}
