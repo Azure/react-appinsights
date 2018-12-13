eab3f611cedebe70dc2e7ba6ed00ae867247b806
Hiral Desai <hidesai@microsoft.com>
2018-12-13 10:51:32 -0800

    Removing access to Instance Adding tsdoc

--- src/IReactAISettings.ts
+++ src/IReactAISettings.ts
@@ -2,9 +2,43 @@
 // Licensed under the MIT License.
 
 import { History } from "history";
+
+/**
+ * Settings to initialize ReactAI instance
+ *
+ * @export
+ * @interface IReactAISettings
+ */
 export default interface IReactAISettings {
+  /**
+   * Application Insights Instrumentation Key
+   *
+   * @type {string}
+   * @memberof IReactAISettings
+   */
   instrumentationKey: string;
+  /**
+   * Context/custom dimensions for initialization
+   * You can also do this post initialization using ReactAI.setContext()
+   *
+   * @type {{ [key: string]: any }}
+   * @memberof IReactAISettings
+   */
   initialContext?: { [key: string]: any };
+  /**
+   * React router history for enabling Application Insights PageView tracking
+   * If not supplied
+   *
+   * @type {History}
+   * @memberof IReactAISettings
+   */
   history?: History;
+  /**
+   * Debug mode
+   * Enable this when developing to see debug messages from the library displayed on the console
+   *
+   * @type {boolean}
+   * @memberof IReactAISettings
+   */
   debug?: boolean;
 }
