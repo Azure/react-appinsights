acb393fcf2bb9100e07a0c91352f209b55128262
Hiral Desai <hidesai@microsoft.com>
2019-01-09 14:18:31 -0800

    Custom engagement tracking implementation

--- src/reactAIWithTracking.tsx
+++ src/reactAIWithTracking.tsx
@@ -1,79 +1,112 @@
 import { IMetricTelemetry } from "@microsoft/applicationinsights-web";
 import * as React from "react";
-import IdleTimer from "react-idle-timer";
-import ReactAI from "./ReactAI";
+import { ReactAI } from ".";
 
-export function reactAIWithTracking<P>(Component: React.ComponentType<P>): React.ComponentClass<P> {  
+export function reactAIWithTracking<P>(Component: React.ComponentType<P>): React.ComponentClass<P> {
   class ReactAIWrappedComponent extends React.Component<P> {
-    private idleTimer?: IdleTimer;
-    private wrappedElement: Node | undefined;
-    private componentName = Component.displayName || Component.prototype.constructor.name;
-    private engagementTimeSeconds: number = 0;
+    private componentName: string = Component.prototype.constructor.name;
+    private mountTimestamp: number = 0;
+    private firstActiveTime: number = 0;
+    private idleStartTime: number = 0;
+    private lastActiveTime: number = 0;
+    private idleTimeMs: number = 0;
+    private idleCount: number = 0;
+    private timeoutMs: number = 5000;
+    private intervalId?: NodeJS.Timeout;
 
-    constructor(props: P) {
-      super(props);
+    public componentDidMount() {
+      this.mountTimestamp = Date.now();
+      this.firstActiveTime = 0;
+      this.idleTimeMs = 0;
+      this.lastActiveTime = 0;
+      this.idleStartTime = 0;
+      this.idleCount = 0;
+
+      this.intervalId = setInterval(() => {
+        if (this.lastActiveTime > 0 && this.idleStartTime === 0 && Date.now() - this.lastActiveTime >= this.timeoutMs) {
+          this.idleStartTime = Date.now();
+          this.idleCount++;
+          this.logIfEnabled("componentDidMount", "Starting idle time.");
+        }
+      }, 100);
     }
 
     public componentWillUnmount() {
-      if (!this.idleTimer) {
-        throw new Error("reactAIWithTracking:unMount: IdleTimer isn't initialized.");
+      if (this.mountTimestamp === 0) {
+        throw new Error("reactAIWithTracking:componentWillUnmount: mountTimestamp isn't initialized.");
       }
 
+      if (!ReactAI.RootInstance) {
+        throw new Error("reactAIWithTracking:componentWillUnmount: ReactAI isn't initialized yet.");
+      }
+
+      if (this.intervalId) {
+        clearInterval(this.intervalId);
+      }
+
+      if (this.firstActiveTime === 0) {
+        this.logIfEnabled("componentWillUnmount", "Nothing to track.");
+        return;
+      }
+
+      const engagementTime = this.getEngagementTimeSeconds();
       const metricData: IMetricTelemetry = {
-        average: this.engagementTimeSeconds,
+        average: engagementTime,
         name: "React Component Engaged Time (seconds)",
         sampleCount: 1
       };
 
       const additionalProperties: { [key: string]: any } = { "Component Name": this.componentName };
-
-      if (!ReactAI.RootInstance) {
-        throw new Error("reactAIWithTracking:unMount: ReactAI isn't initialized yet.");
-      }
-
-      if (ReactAI.IsDebugMode) {
-        console.log("reactAIWithTracking:unMount: tracking engagement time metric: ", metricData, additionalProperties);
-      }
-
+      this.logIfEnabled(
+        "componentWillUnmount",
+        `Tracking ${engagementTime} seconds of engagement time for ${this.componentName}.`
+      );
       ReactAI.RootInstance.trackMetric(metricData, additionalProperties);
     }
 
     public render() {
       return (
-        <div ref={c => (this.wrappedElement = c as Node)}>
-          <IdleTimer
-            ref={(ref: IdleTimer) => {
-              this.idleTimer = ref;
-            }}
-            element={this.wrappedElement}
-            onActive={this.onActive}
-            onIdle={this.onIdle}
-            onAction={this.onAction}
-            timeout={1000 * 60 * 15}
-          />
-          <Component {...this.props}  />
+        <div
+          onKeyDown={this.trackActivity}
+          onMouseMove={this.trackActivity}
+          onScroll={this.trackActivity}
+          onMouseDown={this.trackActivity}
+          onTouchStart={this.trackActivity}
+          onTouchMove={this.trackActivity}
+        >
+          <Component {...this.props} />
         </div>
       );
     }
 
-    private onAction = (e: Event) => {
-      this.engagementTimeSeconds = this.idleTimer ? this.idleTimer.getElapsedTime() / 1000 : 0;
-      if (ReactAI.IsDebugMode) {
-        console.log("reactAIWithTracking:onAction: user is interacting with component.", "Time to idle: ", this.idleTimer ? this.idleTimer.getRemainingTime() : undefined);
+    private trackActivity = (e: React.SyntheticEvent) => {
+      if (this.firstActiveTime === 0) {
+        this.firstActiveTime = Date.now();
+      }
+      this.lastActiveTime = Date.now();
+      if (this.idleStartTime > 0) {
+        const lastIdleTimeMs = Date.now() - this.idleStartTime;
+        this.idleTimeMs += lastIdleTimeMs;
+        this.logIfEnabled("trackActivity", `Idle to active added ${lastIdleTimeMs / 1000} seconds of idle time.`);
+        this.idleStartTime = 0;
       }
     };
 
-    private onActive = (e: Event) => {
-      this.engagementTimeSeconds = this.idleTimer ? this.idleTimer.getElapsedTime() / 1000 : 0;
+    private logIfEnabled = (from: string, message: string) => {
       if (ReactAI.IsDebugMode) {
-        console.log("reactAIWithTracking:onActive: user is interacting with component.", "Time to idle: ", this.idleTimer ? this.idleTimer.getRemainingTime() : undefined);        
+        console.log(`reactAIWithTracking:${this.componentName}:${from}: ${message}`, {
+          engagementTime: this.getEngagementTimeSeconds(),
+          firstActiveTime: this.firstActiveTime,
+          idleStartTime: this.idleStartTime,
+          idleTimeMs: this.idleTimeMs,
+          lastActiveTime: this.lastActiveTime,
+          mountTimestamp: this.mountTimestamp
+        });
       }
     };
 
-    private onIdle = (e: Event) => {
-      if (ReactAI.IsDebugMode) {
-        console.log("reactAIWithTracking:onIdle: user is idle on component.", "Last active time: ", this.idleTimer ? this.idleTimer.getLastActiveTime() : undefined);
-      }
+    private getEngagementTimeSeconds = () => {
+      return (Date.now() - this.firstActiveTime - this.idleTimeMs - this.idleCount * this.timeoutMs) / 1000;
     };
   }
 
