2c20ff4f35101ee6b8245c8a064a771f3b1e9c45
Paolo Viotti <pviotti@microsoft.com>
2019-01-10 10:31:29 +0000

    add copyright header; minor cosmetics

--- src/reactAIWithTracking.tsx
+++ src/reactAIWithTracking.tsx
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 import { IMetricTelemetry } from "@microsoft/applicationinsights-web";
 import * as React from "react";
 import { ReactAI } from ".";
