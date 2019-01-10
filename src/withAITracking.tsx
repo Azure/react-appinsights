3cbf3e1e5f28c304f478916469af2c34ea885910
Paolo Viotti <pviotti@microsoft.com>
2019-01-10 16:07:42 +0000

    fix minor inaccuracy in trackActivity; rename logIfdebug -> debugLog; rename variables for clarity

--- src/withAITracking.tsx
+++ src/withAITracking.tsx
@@ -10,27 +10,27 @@ export default function withAITracking<P>(Component: React.ComponentType<P>): Re
   return class extends React.Component<P> {
     private componentName: string = Component.prototype.constructor.name;
     private mountTimestamp: number = 0;
-    private firstActiveTime: number = 0;
-    private idleStartTime: number = 0;
-    private lastActiveTime: number = 0;
-    private idleTimeMs: number = 0;
+    private firstActiveTimestamp: number = 0;
+    private idleStartTimestamp: number = 0;
+    private lastActiveTimestamp: number = 0;
+    private totalIdleTime: number = 0;
     private idleCount: number = 0;
-    private timeoutMs: number = 5000;
+    private idleTimeout: number = 5000;
     private intervalId?: NodeJS.Timeout;
 
     public componentDidMount() {
       this.mountTimestamp = Date.now();
-      this.firstActiveTime = 0;
-      this.idleTimeMs = 0;
-      this.lastActiveTime = 0;
-      this.idleStartTime = 0;
+      this.firstActiveTimestamp = 0;
+      this.totalIdleTime = 0;
+      this.lastActiveTimestamp = 0;
+      this.idleStartTimestamp = 0;
       this.idleCount = 0;
 
       this.intervalId = setInterval(() => {
-        if (this.lastActiveTime > 0 && this.idleStartTime === 0 && Date.now() - this.lastActiveTime >= this.timeoutMs) {
-          this.idleStartTime = Date.now();
+        if (this.lastActiveTimestamp > 0 && this.idleStartTimestamp === 0 && Date.now() - this.lastActiveTimestamp >= this.idleTimeout) {
+          this.idleStartTimestamp = Date.now();
           this.idleCount++;
-          this.logIfEnabled("componentDidMount", "Starting idle time.");
+          this.debugLog("componentDidMount", "Starting idle time.");
         }
       }, 100);
     }
@@ -48,8 +48,8 @@ export default function withAITracking<P>(Component: React.ComponentType<P>): Re
         clearInterval(this.intervalId);
       }
 
-      if (this.firstActiveTime === 0) {
-        this.logIfEnabled("componentWillUnmount", "Nothing to track.");
+      if (this.firstActiveTimestamp === 0) {
+        this.debugLog("componentWillUnmount", "Nothing to track.");
         return;
       }
 
@@ -61,7 +61,7 @@ export default function withAITracking<P>(Component: React.ComponentType<P>): Re
       };
 
       const additionalProperties: { [key: string]: any } = { "Component Name": this.componentName };
-      this.logIfEnabled(
+      this.debugLog(
         "componentWillUnmount",
         `Tracking ${engagementTime} seconds of engagement time for ${this.componentName}.`
       );
@@ -83,34 +83,37 @@ export default function withAITracking<P>(Component: React.ComponentType<P>): Re
       );
     }
 
-    private trackActivity = (e: React.SyntheticEvent) => {
-      if (this.firstActiveTime === 0) {
-        this.firstActiveTime = Date.now();
+    private trackActivity = (e: React.SyntheticEvent): void => {
+      if (this.firstActiveTimestamp === 0) {
+        this.firstActiveTimestamp = Date.now();
+        this.lastActiveTimestamp = this.firstActiveTimestamp;
+      } else {
+        this.lastActiveTimestamp = Date.now();
       }
-      this.lastActiveTime = Date.now();
-      if (this.idleStartTime > 0) {
-        const lastIdleTimeMs = Date.now() - this.idleStartTime;
-        this.idleTimeMs += lastIdleTimeMs;
-        this.logIfEnabled("trackActivity", `Idle to active added ${lastIdleTimeMs / 1000} seconds of idle time.`);
-        this.idleStartTime = 0;
-      }
-    };
 
-    private logIfEnabled = (from: string, message: string) => {
+      if (this.idleStartTimestamp > 0) {
+        const lastIdleTime = this.lastActiveTimestamp - this.idleStartTimestamp;
+        this.totalIdleTime += lastIdleTime;
+        this.debugLog("trackActivity", `Idle to active added ${lastIdleTime / 1000} seconds of idle time.`);
+        this.idleStartTimestamp = 0;
+      }
+    }
+
+    private debugLog(from: string, message: string): void {
       if (ReactAI.isDebugMode) {
         console.log(`withAITracking:${this.componentName}:${from}: ${message}`, {
           engagementTime: this.getEngagementTimeSeconds(),
-          firstActiveTime: this.firstActiveTime,
-          idleStartTime: this.idleStartTime,
-          idleTimeMs: this.idleTimeMs,
-          lastActiveTime: this.lastActiveTime,
+          firstActiveTime: this.firstActiveTimestamp,
+          idleStartTime: this.idleStartTimestamp,
+          idleTimeMs: this.totalIdleTime,
+          lastActiveTime: this.lastActiveTimestamp,
           mountTimestamp: this.mountTimestamp
         });
       }
-    };
+    }
 
-    private getEngagementTimeSeconds = () => {
-      return (Date.now() - this.firstActiveTime - this.idleTimeMs - this.idleCount * this.timeoutMs) / 1000;
-    };
+    private getEngagementTimeSeconds(): number {
+      return (Date.now() - this.firstActiveTimestamp - this.totalIdleTime - this.idleCount * this.idleTimeout) / 1000;
+    }
   }
 }
