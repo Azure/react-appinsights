b8d12b517117c2780991d237b38cac9e92281af7
Paolo Viotti <pviotti@microsoft.com>
2019-01-23 14:25:00 +0000

    add track page view test; improve context reset test

--- test/ReactAI.test.ts
+++ test/ReactAI.test.ts
@@ -1,6 +1,9 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+import createHistory from 'history/createBrowserHistory';
+
+const IKEY: string = 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxxx';
 
 describe("ReactAI", () => {
 
@@ -16,20 +19,20 @@ describe("ReactAI", () => {
 
   it("initializes correctly", () => {
     const ReactAI = require('../src/ReactAI').default;
-    ReactAI.initialize({ instrumentationKey: "my-i-key" });
+    ReactAI.initialize({ instrumentationKey: IKEY });
     expect(ReactAI.rootInstance).not.toBe(undefined);
-    expect(ReactAI.rootInstance.config.instrumentationKey).toBe("my-i-key");
+    expect(ReactAI.rootInstance.config.instrumentationKey).toBe(IKEY);
   });
 
   it("sets debug mode as expected", () => {
     const ReactAI = require('../src/ReactAI').default;
-    ReactAI.initialize({ instrumentationKey: "my-i-key", debug: true });
+    ReactAI.initialize({ instrumentationKey: IKEY, debug: true });
     expect(ReactAI.isDebugMode).toBe(true);
   });
 
   it("sets context correctly", () => {
     const ReactAI = require('../src/ReactAI').default;
-    ReactAI.initialize({ instrumentationKey: "my-i-key" });
+    ReactAI.initialize({ instrumentationKey: IKEY });
     ReactAI.setContext({ prop1: "value1", prop2: "value2" });
     expect(ReactAI.context.prop1).toBe("value1");
     expect(ReactAI.context.prop2).toBe("value2");
@@ -37,7 +40,9 @@ describe("ReactAI", () => {
 
   it("resets context correctly", () => {
     const ReactAI = require('../src/ReactAI').default;
-    ReactAI.initialize({ instrumentationKey: "my-i-key" });
+    ReactAI.initialize({ instrumentationKey: IKEY });
+    ReactAI.setContext({ prop1: "value1" });
+    expect(ReactAI.context.prop1).toBe("value1");
     ReactAI.setContext({ prop3: "value3" }, true);
     expect(ReactAI.context.prop3).toBe("value3");
     expect(ReactAI.context.prop1).toBe(undefined);
@@ -45,9 +50,29 @@ describe("ReactAI", () => {
 
   it("resets context on initialization", () => {
     const ReactAI = require('../src/ReactAI').default;
-    ReactAI.initialize({ instrumentationKey: "my-i-key", initialContext: { prop1: "value1" } });
+    ReactAI.initialize({ instrumentationKey: IKEY, initialContext: { prop1: "value1" } });
     expect(ReactAI.context.prop1).toBe("value1");
     expect(ReactAI.context.prop2).toBe(undefined);
     expect(ReactAI.context.prop3).toBe(undefined);
+  });
+
+  it('tracks page views', () => {
+    const ReactAI = require('../src/ReactAI').default;
+    const emulatedHistory = createHistory();
+    const initialContext = { prop1: "value1" };
+    ReactAI.initialize({ instrumentationKey: IKEY, initialContext: initialContext, debug: true, history: emulatedHistory });
+    ReactAI.rootInstance.trackPageView = jest.fn();
+    jest.useFakeTimers();
+
+    // emulate change in history
+    emulatedHistory.push('/home', { some: 'state' });
+    emulatedHistory.push('/new-fancy-page');
+    jest.runOnlyPendingTimers();
+
+    const pageViewTelemetry1 = { uri: '/home', properties: initialContext };
+    const pageViewTelemetry2 = { uri: '/new-fancy-page', properties: initialContext };
+    expect(ReactAI.rootInstance.trackPageView).toHaveBeenCalledTimes(2);
+    expect(ReactAI.rootInstance.trackPageView).toHaveBeenNthCalledWith(1, pageViewTelemetry1);
+    expect(ReactAI.rootInstance.trackPageView).toHaveBeenNthCalledWith(2, pageViewTelemetry2);
   });
 });
