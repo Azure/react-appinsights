7f73bfb3541fb93208c2c665757e5579ce7d1bc0
Paolo Viotti <pviotti@microsoft.com>
2019-03-06 15:42:53 +0000

    rm legacy folder

--- legacy/test/setupTests.js
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-configure({ adapter: new Adapter() });
